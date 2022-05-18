f3348e37d5a7dd1406226dbd97a9c8d5991e88d7
Kushagra Agarwal <kushagra666@gmail.com>
2022-05-18 18:47:29 +0530

    Update View.swift

--- Sources/FlipBook/View.swift
+++ Sources/FlipBook/View.swift
@@ -63,7 +63,7 @@ extension View {
     func fb_makeViewSnapshot() -> Image? {
         var rasterizedView: UIImage?
         
-        UIGraphicsBeginImageContextWithOptions(self.bounds.size, true, 1.5)
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, true, 0)
 
         self.drawHierarchy(in: self.bounds, afterScreenUpdates: false)
         rasterizedView = UIGraphicsGetImageFromCurrentImageContext()
